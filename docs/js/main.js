a8262c92418101342385009eaee125b1074dbf69
arifulsajib <allaboutsajib@gmail.com>
2023-11-28 23:05:58 +0600

    done

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,8 @@
+// Navbar toggle
 const toggleBtn = document.querySelector(".toggle-btn");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 toggleBtn.addEventListener("click", () => {
-    dropdownMenu.classList.
+  dropdownMenu.classList.toggle("hidden");
   dropdownMenu.classList.toggle("top-16");
 });
